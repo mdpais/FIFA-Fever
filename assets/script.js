5c909f860cfdd886375b521fc275c4f39c413727
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2022-11-29 21:37:45 -0400

    created a fetch function in the script.js to fetch data from api

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,18 @@
 
 
-var requestUrl = 'https://apiv3.apifootball.com/?action=get_players&player_name=Sergio Camus&APIkey=xxxxxxxxxxxxxx';
+var requestUrl = 'https://apiv3.apifootball.com/?action=get_teams&league_id=302&APIkey=11c5aab5efe97256e5343fe4bb3dbb3cf1dff45f2c409325ed773837fcdd51d1';
+//var apiKey = '11c5aab5efe97256e5343fe4bb3dbb3cf1dff45f2c409325ed773837fcdd51d1';
+
 fetch(requestUrl)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
     console.log(data);
+    for (var i = 0; i < data.length; i++) {
+      console.log(data[i].url);
+      //console.log(data[i].user.login);
+    }
   });
+
+  
