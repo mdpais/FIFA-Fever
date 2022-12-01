09d37f2e55e492e51e7fe9172405102c3ae750e9
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2022-12-01 18:53:20 -0400

    added the api and api key for statistics of players

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,27 @@
 
 
-var requestUrl = 'https://apiv3.apifootball.com/?action=get_teams&league_id=302&APIkey=11c5aab5efe97256e5343fe4bb3dbb3cf1dff45f2c409325ed773837fcdd51d1';
+var playersUrl = 'https://apiv3.apifootball.com/?action=get_teams&league_id=302&APIkey=11c5aab5efe97256e5343fe4bb3dbb3cf1dff45f2c409325ed773837fcdd51d1';
+var apiURL = 'https://apiv3.apifootball.com/?action=get_statistics&match_id=86392&APIkey=11c5aab5efe97256e5343fe4bb3dbb3cf1dff45f2c409325ed773837fcdd51d1';
 //var apiKey = '11c5aab5efe97256e5343fe4bb3dbb3cf1dff45f2c409325ed773837fcdd51d1';
 
-fetch(requestUrl)
+fetch(playersUrl)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
     console.log(data);
-    for (var i = 0; i < data.length; i++) {
-      console.log(data[i].url);
+    //for (var i = 0; i < data.length; i++) {
+      //console.log(data[i].url);
       //console.log(data[i].user.login);
-    }
   });
+  //});
 
-  
+  var statisticURL = 'https://apiv3.apifootball.com/?action=get_statistics&match_id=86392&APIkey=11c5aab5efe97256e5343fe4bb3dbb3cf1dff45f2c409325ed773837fcdd51d1';
+
+  fetch(statisticURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
